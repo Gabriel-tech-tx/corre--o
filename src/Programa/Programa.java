d05a695c375d0a6173020e991779c305fe0152cf
Gabriel <gabrielmattostx@gmail.com>
2021-03-18 20:26:08 -0300

    Primeiro commit

---
+++ src/Programa/Programa.java
@@ -0,0 +1,68 @@
+package Programa;
+
+import java.util.Scanner;
+
+import Classes.Cliente;
+import Classes.Conta;
+
+public class Programa {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Conta conta = new Conta();
+        Cliente cliente = new Cliente("Gab", conta);
+
+        int opcao = -1;
+
+        while (opcao !=0){
+            exibeMenu();
+            opcao = ler.nextInt();
+
+            switch(opcao){
+                case 0:
+                 System.out.println("Encerrando a aplicação");
+                 return;
+                case 1:
+                 sacar(ler, cliente);
+                 break;
+                case 2:
+                 depositar(ler, cliente);
+                 break;
+                case 3:
+                 exibeSaldo(cliente);
+                 break;
+                default:
+                 System.out.println("Operação invalida. Verifique");
+            }
+
+        }
+
+        ler.close();
+    }
+
+    private static void exibeSaldo(Cliente cliente) {
+       cliente.exibirSaldo();
+    }
+
+    private static void depositar(Scanner ler, Cliente cliente) {
+        double valor;
+        System.out.println("Digite o valor a depositar");
+         valor = ler.nextDouble();
+        cliente.depositar(valor);;
+    }
+
+    private static void sacar(Scanner ler, Cliente cliente) {
+        double valor;
+        System.out.println("Digite o valor a sacar");
+         valor = ler.nextDouble();
+         cliente.sacar(valor);
+    }
+
+    private static void exibeMenu() {
+        System.out.println("\n*****************");
+        System.out.println("1 - saque");  
+        System.out.println("2 - depósito");
+        System.out.println("3 - saldo");
+        System.out.println("0 - sair");
+        System.out.println("\nDigite a opção desejada");
+    }
+}
